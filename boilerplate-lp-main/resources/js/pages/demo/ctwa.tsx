a8ae9f5c6a9385e28f40127e0a0bfa2167e76751
Septian Fahmi Ardiansyah <fahrisepti888@gmail.com>
2026-09-12 14:02:50 +0700

    [FIX] Remove .DS_Store, add .gitignore, include lms-showcase.mp4, and enable smooth scrolling in CSS

--- boilerplate-lp-main/resources/js/pages/demo/ctwa.tsx
+++ boilerplate-lp-main/resources/js/pages/demo/ctwa.tsx
@@ -1560,7 +1560,7 @@ export default function LandingPage() {
                             className="[display:block] [aspect-ratio:16/9] [width:100%] [object-fit:cover] [background:#151515]"
                         >
                             <source
-                                src="https://demo-fullbright.b-cdn.net/NEW.mp4#t=4"
+                                src="/assets/lms-showcase.mp4#t=4"
                                 type="video/mp4"
                             />
                             Browser kamu tidak mendukung pemutaran video.
